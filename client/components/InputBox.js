9ecd59c57a84b1ea683c0d3c7eec28fbd4439cfb
Justin Salim <justinsalim73@gmail.com>
2024-05-28 16:26:42 +0700

    Update app.json and InputBox component

--- client/components/InputBox.js
+++ client/components/InputBox.js
@@ -1,32 +1,49 @@
-import { View, Text, StyleSheet, TextInput } from "react-native";
-import React from "react";
+import React from 'react';
+import { View, StyleSheet, TextInput } from 'react-native';
+import { Foundation } from '@expo/vector-icons';
 
-const InputBox = ({ inputTitle, keyboardType, autoComplete, secureTextEntry=false, placeholder, value, setValue}) => {
+const InputBox = ({
+  iconName,
+  iconSize,
+  iconColor,
+  keyboardType,
+  autoComplete,
+  secureTextEntry = false,
+  placeholder,
+  value,
+  setValue,
+}) => {
   return (
-    <View>
-      <Text>{inputTitle}</Text>
-      <TextInput style={styles.inputBox} placeholder={placeholder}
-      autoCorrect={false}
-      keyboardType={keyboardType}
-      autoComplete={autoComplete}
-      secureTextEntry={secureTextEntry}
-      value={value}
-      onChangeText={(text) => setValue(text)}
+    <View style={styles.container}>
+      <Foundation name={iconName} size={iconSize} color={iconColor} style={styles.icon} />
+      <TextInput
+        style={styles.input}
+        placeholder={placeholder}
+        autoCorrect={false}
+        keyboardType={keyboardType}
+        autoComplete={autoComplete}
+        secureTextEntry={secureTextEntry}
+        value={value}
+        onChangeText={(text) => setValue(text)}
       />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  inputBox: {
-    height: 40,
-    marginBottom: 20,
-    backgroundColor: "white",
-    borderRadius: 10,
-    borderWidth: 1,
-    borderColor: "black",
+  container: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    borderBottomWidth: 1,
+    borderBottomColor: 'gray',
+    padding: 5,
+  },
+  icon: {
+    marginRight: 10,
+  },
+  input: {
+    flex: 1,
     padding: 10,
-    marginTop: 5,
   },
 });
 
--- client/screens/auth/Register.js
+++ client/screens/auth/Register.js
@@ -1,47 +1,85 @@
-import { View, Text, StyleSheet, TextInput } from 'react-native';
-import React,{useState} from 'react';
+import { View, Text, StyleSheet, TouchableOpacity, ScrollView } from 'react-native';
+import React, { useState } from 'react';
 import InputBox from '../../components/InputBox';
 
 const Register = () => {
-  const [name,setName] = useState('');
-  const [email,setEmail] = useState('');
-  const [password,setPassword] = useState('');
-  const [confirmPassword,setConfirmPassword] = useState('');
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+
   return (
     <View style={styles.container}>
-      <Text style={styles.pageTitle}>Register</Text>
-      <View style={{ marginHorizontal: 20 }}>
-        <InputBox inputTitle={"Name"} value={name} setValue={setName}/>
-        <InputBox inputTitle={"Email"} keyboardType={"email-address"} autoComplete={"email"} value={email} setValue={setEmail}/>
-        <InputBox inputTitle={"Password"} secureTextEntry={true} autoComplete={"password"} value={password} setValue={setPassword}/>
-        <InputBox inputTitle={"Confirm Password"} secureTextEntry={true} autoComplete={"password"} value={confirmPassword} setValue={setConfirmPassword}/>
-      </View>
-      <Text>{JSON.stringify({name,email,password,confirmPassword}, null, 4)}</Text>
+      <ScrollView contentContainerStyle={styles.scrollContent}>
+        <Text style={styles.pageTitle}>Register</Text>
+        <Text style={styles.pageTitleDesc}>Create an account to continue.</Text>
+        <View style={{ marginHorizontal: 20 }}>
+          <InputBox iconName="torso" iconSize={22} iconColor={"grey"} value={name} setValue={setName} placeholder={'FULL NAME'} />
+          <InputBox iconName="mail" iconSize={22} iconColor={"grey"} keyboardType={"email-address"} autoComplete={"email"} value={email} setValue={setEmail} placeholder={'EMAIL'} />
+          <InputBox iconName="lock" iconSize={22} iconColor={"grey"} secureTextEntry={true} autoComplete={"password"} value={password} setValue={setPassword} placeholder={'PASSWORD'} />
+          <InputBox iconName="lock" iconSize={22} iconColor={"grey"} secureTextEntry={true} autoComplete={"password"} value={confirmPassword} setValue={setConfirmPassword} placeholder={'CONFIRM PASSWORD'} />
+          <Text style={styles.link} onPress={() => {}}>or login here</Text>
+          <View style={styles.buttonContainer}>
+            <TouchableOpacity style={styles.button} onPress={() => {}}>
+              <Text style={styles.buttonText}>REGISTER</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </ScrollView>
     </View>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-    },
-    pageTitle: {
-        fontSize: 40,
-        fontWeight: 'bold',
-        marginBottom: 20,
-        textAlign: 'center'
-    },
-    input: {
-        height: 40,
-        marginBottom: 20,
-        backgroundColor: 'white',
-        borderRadius: 10,
-        borderWidth: 1,
-        borderColor: 'black',
-        padding: 10,
-        marginTop: 5
-    }
-})
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    marginLeft: 10,
+  },
+  scrollContent: {
+    flexGrow: 1,
+    justifyContent: 'center',
+  },
+  pageTitle: {
+    fontFamily: 'interBlack',
+    fontSize: 35,
+    fontWeight: 'bold',
+    marginBottom: 5,
+    textAlign: 'left',
+    marginLeft: 20,
+  },
+  pageTitleDesc: {
+    fontFamily: 'interExtraLight',
+    fontSize: 15,
+    marginBottom: 20,
+    textAlign: 'left',
+    marginLeft: 20,
+    fontStyle: 'extra light',
+  },
+  link: {
+    marginTop: 10,
+    marginBottom: 10, // Reduce marginBottom to move the button closer
+    color: 'red',
+    textDecorationLine: 'underline',
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    justifyContent: 'flex-end',
+    marginTop: 10, // Reduce marginTop to move the button closer to the input fields
+    marginRight: 20,
+  },
+  button: {
+    backgroundColor: 'red',
+    borderRadius: 15, // Adjust as needed
+    paddingVertical: 10,
+    paddingHorizontal: 30,
+    marginTop: 10,
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 16,
+    textAlign: 'center',
+  },
+});
 
-export default Register
+export default Register;
