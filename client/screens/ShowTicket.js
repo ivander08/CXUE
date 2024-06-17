200c684cd017e3c074302e3810c61013930aa3a6
Scorkinn <ruben.tobiachaiyadi@yahoo.com>
2024-06-17 18:14:18 +0700

    Added gacha screen stuff idk

--- client/screens/ShowTicket.js
+++ client/screens/ShowTicket.js
@@ -1,15 +1,66 @@
-import React from 'react';
-import { View, Text, StyleSheet, Image, ImageBackground, TouchableOpacity } from 'react-native';
-import Svg, { Path } from "react-native-svg";
-const buttonImage = require("../assets/images/btn-onboard.png");
+import React, { useEffect, useState } from 'react';
+import { View, Text, StyleSheet, Image, ImageBackground, TouchableOpacity, Modal, Button } from 'react-native';
+import { BlurView } from 'expo-blur';
+import Svg, { Path, } from "react-native-svg";
+import { MaterialIcons } from "@expo/vector-icons";
 import Navbar from "../components/navbar";
-import { useNavigation } from '@react-navigation/native';
+import { CommonActions } from '@react-navigation/native';
 
-const ShowTicket = (route ) => {
-    const navigation = useNavigation();
+const PrizeInText = ({ text }) => {
+    return (
+    <View style={styles.prizeBorder}>
+        <MaterialIcons name="card-giftcard" size={24} color="#D8C764" />
+        <Text style={styles.prizeText}>{text}</Text>
+    </View>
+    );
+};
+
+const PrizeWithImage = ({ item, image}) => {
+    return (
+        <View style={styles.centeredView}>
+            <Image source={image} />
+            <Text style={styles.prizeTextImage}>{item}</Text>
+        </View>
+    
+    );
+};
+
+function getRandomReward() {
+    const randomNumber = Math.random();
+    if (randomNumber < 0.4) {
+        return <PrizeInText text="1x Free Drink" />
+    }  else if (randomNumber < 0.2) {
+        return <PrizeInText text="1x Free Ticket" />
+    } else {
+        return <PrizeWithImage item="PARASITE X CXUE T-SHIRT" image={require("../assets/images/ParasiteTShirt.png")} />
+    }
+}
+
+const ShowTicket = ( {navigation} ) => {
+    const [ModalVisible, setModalVisible] = useState(false);
+    const [modalContent, setModalContent] = useState(null);
+    const backtoHome = () => {
+        navigation.dispatch(
+            CommonActions.reset({
+              index: 0,
+              routes: [
+                { name: 'Home' },
+                { name: 'Navbar' },
+              ],
+            })
+          );
+    }
+
+    useEffect(() => {
+        const show = Math.random() < 0.5; // Adjust probability
+        if (show) {
+            setModalContent(getRandomReward());
+            setModalVisible(true);
+        }
+    }, []);
+
     return (
         <View style={styles.container}>
-
             <ImageBackground style={styles.movieImgBackground} source={require("../assets/images/Parasite.jpg")}>
                 <View style={styles.overlay}/>
                     <Svg width="318" height="444" viewBox="0 0 318 444">
@@ -68,12 +119,59 @@ const ShowTicket = (route ) => {
                         
                     </Svg>
                     <TouchableOpacity style={styles.button}
-                        onPress={() => navigation.navigate('Navbar')}
+                        onPress={backtoHome}
                         >
                         <Text style={styles.buttonText}>Go Home</Text>
                         </TouchableOpacity>
                 
             </ImageBackground>
+            <Modal
+                animationType="fade"
+                transparent={true}
+                visible={ModalVisible}
+                onRequestClose={() => setModalVisible(false)}
+            >
+                <View style={styles.centeredView}>
+                    <BlurView intensity={30} tint="dark" style={styles.fullScreen} 
+                    experimentalBlurMethod='dimezisBlurView' //Kadang bikin nge-lag
+                    >
+                        <View style={styles.modalBackgroundColor}>
+                            {/*experimentalBlurMethod='dimezisBlurView'*/}
+                            {/* It's logo if want*/}
+                            {/* <View style={{ flexDirection: "row", alignItems: "center", position: "absolute", top:10}}>
+                                <Image
+                                source={require("../assets/images/logo.png")}
+                                style={{ width: 33, height: 33, marginHorizontal: 10}}
+                                />
+                                <Text
+                                style={{
+                                    fontFamily: "interBlack",
+                                    fontStyle: "italic",
+                                    fontWeight: "900",
+                                    fontSize: 22.5,
+                                    color: "#fefefe",
+                                    marginHorizontal: 5
+                                }}
+                                >
+                                CXUE
+                                </Text>
+                            </View> */} 
+                            <View style={styles.div}>
+                                <Image source={require("../assets/images/gift.png")}/>
+                                <Text style={styles.modalTextBig}>Congratulations!</Text>
+                                <Text style={styles.modalText}>You won a special prize</Text>
+                            </View>
+                            <View style={styles.prizeContent}>
+                                {modalContent}
+                            </View>
+                            <View>
+                                <Button title="Close" onPress={() => setModalVisible(false)} />
+                            </View>
+                        </View>
+                    </BlurView>
+                </View>
+
+            </Modal>
         </View>
     );
 };
@@ -200,8 +298,78 @@ const styles = StyleSheet.create({
     barcode:{
         alignSelf: 'center',
         marginVertical: 25,
-    }
+    },
+    fullScreen: {
+        // flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: '100%',
+        height: '100%',
+    },
+    modalBackgroundColor:{
+        position: 'absolute',
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: '100%',
+        height: '100%',
+        backgroundColor: 'rgba(13, 22, 11, 0.8)',
+        // width: '80%',
+    },
+    centeredView: {
+        // flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        // backgroundColor: '#0D160B'
+        // marginTop: 22,
+    },
 
+    div: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginBottom: 10,
+        backgroundColor: 'transparent',
+    },
+
+    modalTextBig: {
+        color: 'white',
+        fontWeight: 'bold',
+        fontSize: 35,
+        fontStyle: 'italic',
+        fontSize: 35,
+    },
+    modalText: {
+        color: 'white',
+        // fontWeight: 'bold',
+        fontSize: 15,
+    },
+    prizeBorder:{
+        borderWidth: 1,
+        borderRadius: 10,
+        borderColor: '#D8C764',
+        backgroundColor: '#182017',
+        borderRadius: 15,
+        padding: 10,
+        flexDirection: 'row',
+    },
+    prizeText:{
+        color: 'white',
+        fontSize: 20,
+    },
+    prizeContent:{
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginVertical: 40,
+    },
+    iconCircle:{
+        width: 100,
+        height: 100,
+        borderRadius: 50, // Half of the width and height to create a circle
+        backgroundColor: 'blue', // Example background color
+    },
+    prizeTextImage:{
+        color: '#D8C764',
+        fontSize: 20,
+    }
 });
 
 export default ShowTicket;
